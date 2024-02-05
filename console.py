e49331551f69cfb08ae9e614c46541940899160d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-02-06 00:16:57 +0100

    cleaning circular import in console

--- console.py
+++ console.py
@@ -3,7 +3,7 @@
 
 import cmd
 import shlex
-import models
+from models import storage
 from models.accessory import Accessory
 from models.basemodel import BaseModel
 from models.bill import Bill
@@ -86,8 +86,8 @@ class HBNBCommand(cmd.Cmd):
         if args[0] in classes:
             if len(args) > 1:
                 key = args[0] + "." + args[1]
-                if key in models.storage.all():
-                    print(models.storage.all()[key])
+                if key in storage.all():
+                    print(storage.all()[key])
                 else:
                     print("** no instance found **")
             else:
@@ -103,9 +103,9 @@ class HBNBCommand(cmd.Cmd):
         elif args[0] in classes:
             if len(args) > 1:
                 key = args[0] + "." + args[1]
-                if key in models.storage.all():
-                    models.storage.all().pop(key)
-                    models.storage.save()
+                if key in storage.all():
+                    storage.all().pop(key)
+                    storage.save()
                 else:
                     print("** no instance found **")
             else:
@@ -118,9 +118,9 @@ class HBNBCommand(cmd.Cmd):
         args = shlex.split(arg)
         obj_list = []
         if len(args) == 0:
-            obj_dict = models.storage.all()
+            obj_dict = storage.all()
         elif args[0] in classes:
-            obj_dict = models.storage.all(classes[args[0]])
+            obj_dict = storage.all(classes[args[0]])
         else:
             print("** class doesn't exist **")
             return False
@@ -143,7 +143,7 @@ class HBNBCommand(cmd.Cmd):
         elif args[0] in classes:
             if len(args) > 1:
                 k = args[0] + "." + args[1]
-                if k in models.storage.all():
+                if k in storage.all():
                     if len(args) > 2:
                         if len(args) > 3:
                             if args[0] in cols:
@@ -157,8 +157,8 @@ class HBNBCommand(cmd.Cmd):
                                         args[3] = float(args[3])
                                     except:
                                         args[3] = 0.0
-                            setattr(models.storage.all()[k], args[2], args[3])
-                            models.storage.all()[k].save()
+                            setattr(storage.all()[k], args[2], args[3])
+                            storage.all()[k].save()
                         else:
                             print("** value missing **")
                     else:
