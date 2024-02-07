bd41f938356ee1ff552fdcf952c44fd5336b4a95
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-02-07 16:20:10 +0100

    cleaning circular import in access

--- models/accessory.py
+++ models/accessory.py
@@ -20,4 +20,4 @@ class Accessory(BaseModel, Base):
     consultation_id = Column(Integer, ForeignKey("consultations.id"), nullable=False)
     name = Column(String(60), nullable=False)
     quantity = Column(Integer)
-    status = Column(VARCHAR(50), Enum(OrderStatus))
+    status = Column(VARCHAR(50), Enum(OrderStatus, length=50))
