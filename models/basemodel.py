9dba13b1343d3f5fcf7690689059f90006616ccd
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-02-07 16:39:18 +0100

    cleaning circular import in basemodel

--- models/basemodel.py
+++ models/basemodel.py
@@ -1,53 +1,42 @@
-#!/usr/bin/python3
-"""
-Contains the base for other class
-"""
-
-
 from datetime import datetime
 from sqlalchemy import Column, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
-class BaseModel:
+class BaseModel(Base):
     """The base class for the tables"""
-    def default_value():
-        """To start id from 1"""
-        return 1
-    
+    __abstract__ = True  # Set abstract attribute to True
+
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    @staticmethod
-    def validate_id_value(value):
-        """To ensure id value is a 6-digit"""
+    def validate_id_value(self, value):
+        """To ensure id value is a positive integer"""
         return max(1, min(value, 999999))
-    
+
     @property
     def id(self):
         """to get the id"""
         return self._id
-    
+
     @id.setter
     def id(self, value):
         """To set id to the 6 digits"""
         self._id = self.validate_id_value(value)
-    
+
     def __str__(self):
         """Format id with the leading zeros"""
         return f"{self.id:06d}"
-    
-    def save(self):
-        from models import storage
-        """updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.utcnow()
-        storage.new(self)
-        storage.save()
 
-    def delete(self):
-        from models import storage
+    def save(self, session):
+        """updates the attribute 'updated_at' with the current datetime and saves the instance"""
+        self.updated_at = datetime.utcnow()
+        session.add(self)
+        session.commit()
+
+    def delete(self, session):
         """deletes a current instance"""
-        storage.delete(self)
+        session.delete(self)
+        session.commit()
