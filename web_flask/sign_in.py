011f14e2c904a859c738c21964f79006458be648
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-02-18 23:21:15 +0100

    signs

--- web_flask/sign_in.py
+++ web_flask/sign_in.py
@@ -48,4 +48,4 @@ def login():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5005, debug=True)
+    app.run(host='52.91.202.165', port=5005, debug=True)
--- web_flask/sign_up.py
+++ web_flask/sign_up.py
@@ -45,4 +45,4 @@ def register():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5005, debug=True)
+    app.run(host='52.91.202.165', port=5005, debug=True)
